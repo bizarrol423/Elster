dbc381b5375b18abb408cf337137e8c027c59008
Михаил Пономарёв <michail@MacBook-Air-Mihail.local>
2024-03-20 09:20:04 +0700

    start_project

--- code/main.py
+++ code/main.py
@@ -1,5 +1,14 @@
 import requests
 import func
+from data import token
+
 
 if __name__ == '__main__':
-    print("start project")
+    SearchedOrganization = "театры"
+    url = "https://search-maps.yandex.ru/v1/"+\
+    f"?apikey={token}"+\
+    f"&text={SearchedOrganization}"+\
+    "&lang=ru_RU"+\
+    "&type=biz"+\
+    "&results=500"
+    print(requests.get(url=url).text)
